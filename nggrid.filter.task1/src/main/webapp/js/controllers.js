e4c37be7edf0b944aa5b0b85d7406c3b6da9691d
gdenisenko <denisenko.gennadiy@gmail.com>
2014-04-22 18:50:53 +0300

    -

--- nggrid.filter.task1/src/main/webapp/js/controllers.js
+++ nggrid.filter.task1/src/main/webapp/js/controllers.js
@@ -1,17 +1,18 @@
 myApp.controller('rxmsCntrl', ['$scope', '$location', function($scope, $location) {
-	$scope.myData = [{id : 1, rxmGroupName : 'A', rxmGroup:'SGC30031', rxmName : 'E', rxmNumber:'C0123450', dailyNetDV01 : '12000', dailyCrossDV01 : '25', aggNetDV01 : '$400,000.0'},
+	/*
+	$scope.myData1 = [{id : 1, rxmGroupName : 'A', rxmGroup:'SGC30031', rxmName : 'E', rxmNumber:'C0123450', dailyNetDV01 : '12000', dailyCrossDV01 : '25', aggNetDV01 : '$400,000.0'},
 	                 {id : 2, rxmGroupName : 'B', rxmGroup:'SGC30032', rxmName : 'F', rxmNumber:'C0123459', dailyNetDV01 : '7000', dailyCrossDV01 : '100', aggNetDV01 : '$600,000.0'},
 	                 {id : 3, rxmGroupName : 'C', rxmGroup:'SGC30023', rxmName : 'G', rxmNumber:'C0123451', dailyNetDV01 : '20000', dailyCrossDV01 : '500', aggNetDV01 : '$100,000.0'},
 	                 {id : 4, rxmGroupName : 'D', rxmGroup:'SGC30034', rxmName : 'F', rxmNumber:'C0123448', dailyNetDV01 : '3000', dailyCrossDV01 : '1200', aggNetDV01 : '$400,000.0'},
 	                 {id : 5, rxmGroupName : 'A', rxmGroup:'SGC30025', rxmName : 'G', rxmNumber:'C0123442', dailyNetDV01 : '5000', dailyCrossDV01 : '3300', aggNetDV01 : '$400,000.0'},
 	                 {id : 6, rxmGroupName : 'A', rxmGroup:'SGC30039', rxmName : 'G', rxmNumber:'C0123457', dailyNetDV01 : '12056', dailyCrossDV01 : '70', aggNetDV01 : '$500,000.0'}];
-	
-	
+	*/
+	$scope.myData = rxmsDataArr;
 	$scope.gridOptions = {
 			data : 'myData',
-			columnDefs : [{field : 'rxmGroupName', displayName : 'RXM Group Name', width : 145},
-			              {field : 'rxmGroup', displayName : 'RXM Group Number', width : 155},
-			              {field : 'rxmName', displayName : 'RXM Name'},
+			columnDefs : [{field : 'rxmGroupName', displayName : 'RXM Group Name', width : 250},
+			              {field : 'rxmGroup', displayName : 'RXM Group Number', width : 100},
+			              {field : 'rxmName', displayName : 'RXM Name', width : 250},
 			              {field : 'rxmNumber', displayName : 'RXM Number'},
 			              {field : 'dailyNetDV01', displayName : 'Daily Net DV01', cellTemplate : 'templates/linked_edit_cell.html'},
 			              {field : 'dailyCrossDV01', displayName : 'Daily Cross DV01', cellTemplate : 'templates/linked_edit_cell.html'},
@@ -30,6 +31,7 @@ myApp.controller('rxmsCntrl', ['$scope', '$location', function($scope, $location
 	};
 	
 	///////////////////////////////
+	/*
 	$scope.edit = new Array($scope.gridOptions.data.length);
 	$scope.editSave = function (row) {
 		$scope.edit[row.getProperty('id')] = !$scope.edit[row.getProperty('id')];
@@ -38,7 +40,7 @@ myApp.controller('rxmsCntrl', ['$scope', '$location', function($scope, $location
 	$scope.isEdit = function(row) {
 		return !!$scope.edit[row.getProperty('id')];
 	};
-	
+	*/
 	///////////////////////////////
 	
 	$scope.applyFilter = function() {
@@ -50,7 +52,7 @@ myApp.controller('rxmsCntrl', ['$scope', '$location', function($scope, $location
 }]);
 
 myApp.controller('rxmDetailsCntrl', ['$scope', '$routeParams', function($scope, $routeParams) {	
-	$scope.rxmNumber = $routeParams.rxmNumber;
+	rxmNumber = $routeParams.rxmNumber;
 	isEdit = $routeParams.mode == 'edit';
 	
 	$scope.myData = rxmDetailsData;
@@ -62,7 +64,7 @@ myApp.controller('rxmDetailsCntrl', ['$scope', '$routeParams', function($scope, 
 			showFilter: true,
 			multiSelect: false,
 			showColumnMenu:true,
-			filterOptions: {filterText:'', useExternalFilter: false}, 
+			filterOptions: {filterText:'RXM Number:' + rxmNumber, useExternalFilter: false}, 
 			columnDefs: [      
 		                    { width : 170, cellTemplate: '<input type="button" value="Restrict">&nbsp;<input type="button" value="Edit">&nbsp;<input type="button" value="Kill">'},
 		                    { field: "A_CCY", displayName:'Currency',width: 75},
