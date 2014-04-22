f9891f394e0abe6630abdd5050419e05feba138c
gdenisenko <denisenko.gennadiy@gmail.com>
2014-04-22 15:01:53 +0300

    filter changed

--- nggrid.filter.task1/src/main/webapp/js/controllers.js
+++ nggrid.filter.task1/src/main/webapp/js/controllers.js
@@ -1,15 +1,17 @@
 myApp.controller('rxmsCntrl', ['$scope', '$location', function($scope, $location) {
-	$scope.myData = [{id : 1, rxmGroup:'SGC30031', rxmNumber:'C0123450', dailyNetDV01 : '12000', dailyCrossDV01 : '25', aggNetDV01 : '$400,000.0'},
-	                 {id : 2, rxmGroup:'SGC30032', rxmNumber:'C0123459', dailyNetDV01 : '7000', dailyCrossDV01 : '100', aggNetDV01 : '$600,000.0'},
-	                 {id : 3, rxmGroup:'SGC30033', rxmNumber:'C0123451', dailyNetDV01 : '20000', dailyCrossDV01 : '500', aggNetDV01 : '$100,000.0'},
-	                 {id : 4, rxmGroup:'SGC30034', rxmNumber:'C0123458', dailyNetDV01 : '3000', dailyCrossDV01 : '1200', aggNetDV01 : '$400,000.0'},
-	                 {id : 5, rxmGroup:'SGC30035', rxmNumber:'C0123452', dailyNetDV01 : '5000', dailyCrossDV01 : '3300', aggNetDV01 : '$400,000.0'},
-	                 {id : 6, rxmGroup:'SGC30039', rxmNumber:'C0123457', dailyNetDV01 : '12056', dailyCrossDV01 : '70', aggNetDV01 : '$500,000.0'}];
+	$scope.myData = [{id : 1, rxmGroupName : 'A', rxmGroup:'SGC30031', rxmName : 'E', rxmNumber:'C0123450', dailyNetDV01 : '12000', dailyCrossDV01 : '25', aggNetDV01 : '$400,000.0'},
+	                 {id : 2, rxmGroupName : 'B', rxmGroup:'SGC30032', rxmName : 'F', rxmNumber:'C0123459', dailyNetDV01 : '7000', dailyCrossDV01 : '100', aggNetDV01 : '$600,000.0'},
+	                 {id : 3, rxmGroupName : 'C', rxmGroup:'SGC30023', rxmName : 'G', rxmNumber:'C0123451', dailyNetDV01 : '20000', dailyCrossDV01 : '500', aggNetDV01 : '$100,000.0'},
+	                 {id : 4, rxmGroupName : 'D', rxmGroup:'SGC30034', rxmName : 'F', rxmNumber:'C0123448', dailyNetDV01 : '3000', dailyCrossDV01 : '1200', aggNetDV01 : '$400,000.0'},
+	                 {id : 5, rxmGroupName : 'A', rxmGroup:'SGC30025', rxmName : 'G', rxmNumber:'C0123442', dailyNetDV01 : '5000', dailyCrossDV01 : '3300', aggNetDV01 : '$400,000.0'},
+	                 {id : 6, rxmGroupName : 'A', rxmGroup:'SGC30039', rxmName : 'G', rxmNumber:'C0123457', dailyNetDV01 : '12056', dailyCrossDV01 : '70', aggNetDV01 : '$500,000.0'}];
 	
 	
 	$scope.gridOptions = {
 			data : 'myData',
-			columnDefs : [{field : 'rxmGroup', displayName : 'RXM Group'}, 
+			columnDefs : [{field : 'rxmGroupName', displayName : 'RXM Group Name'},
+			              {field : 'rxmGroup', displayName : 'RXM Group Number'},
+			              {field : 'rxmName', displayName : 'RXM Name'},
 			              {field : 'rxmNumber', displayName : 'RXM Number'},
 			              {field : 'dailyNetDV01', displayName : 'Daily Net DV01', cellTemplate : 'templates/linked_edit_cell.html'},
 			              {field : 'dailyCrossDV01', displayName : 'Daily Cross DV01', cellTemplate : 'templates/linked_edit_cell.html'},
@@ -41,10 +43,17 @@ myApp.controller('rxmsCntrl', ['$scope', '$location', function($scope, $location
 	///////////////////////////////
 	
 	$scope.applyFilter = function() {
-		$scope.gridOptions.filterOptions.filterText = 'Daily Net DV01:' + $scope.dvfilter;
-	}
+		$scope.gridOptions.filterOptions.filterText = 'RXM Group Name:' + getStringValue($scope.rxmGroupNameFilter)  
+													+ ';RXM Group Number:' + getStringValue($scope.rxmGroupNumberFilter)
+													+ ';RXM Name:' + getStringValue($scope.rxmNameFilter)
+													+ ';RXM Number:' + getStringValue($scope.rxmNumberFilter);
+	};
 }]);
 
 myApp.controller('rxmDetailsCntrl', ['$scope', '$routeParams', function($scope, $routeParams) {	
 	$scope.rxmNumber = $routeParams.rxmNumber;
 }]);
+
+function getStringValue(value) {
+	return value ? value : '';
+}
